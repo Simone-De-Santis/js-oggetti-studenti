f37f8c6c7f3c69089f5e6a76f6158fb59272faad
Simone de santis <simone.agentelazio@yahoo.it>
2021-07-19 13:49:27 +0200

    inizio esercizio

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*
+Descrizione:
+ES 1
+Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+Stampare a schermo (in pagina) attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+ES 2
+Creare un array di oggetti di studenti.
+Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+Consigli del giorno:
+Iniziate col riflettere sugli step che dovete eseguire, scrivendoli in italiano sotto forma di commenti
+Facciamo un passo alla volta e verifichiamo i nostri passi col console.log.
+Quando tutto funziona col console.log possiamo preoccuparci di eseguire in pagina!
+ */
