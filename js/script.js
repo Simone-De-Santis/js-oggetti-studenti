be58b842c67113bc84e2755e9dd0c83ff301e9b9
Simone de santis <simone.agentelazio@yahoo.it>
2021-07-20 19:18:26 +0200

    creata la funzione e sostituita  logica di print

--- js/script.js
+++ js/script.js
@@ -41,11 +41,59 @@ var classroom = [
 ];
 
 //? cicliamo nell'array
-var content2 = "";
-for (var i = 0; i < classroom.length; i++) {
-  var currentStudent = classroom[i];
-  content2 += "<li>" + "<span>Nome :  </span>" + currentStudent.name + "   " + "<span>Cognome :</span>" + currentStudent.surname + "</li>";
-}
+// var content2 = "";
+// for (var i = 0; i < classroom.length; i++) {
+//   var currentStudent = classroom[i];
+//   //* stampiamo nome e cognome
+
+//   // content2 += "<li>" + "<span>Nome :  </span>" + currentStudent.name + "   " + "<span>Cognome :</span>" + currentStudent.surname + "</li>";
+//   //* stampiamo tutte le proprietà
+
+//   content2 += "<li>";
+//   //* cicliamo su tutte le preprieta per ogni aray
+//   for (var key in currentStudent) {
+//     content2 += "<span>" + key + ":</span>" + currentStudent[key] + "<br/>";
+//   }
+//   content2 += "</li>";
+// }
 var display = document.getElementById("student-property-2");
-display.innerHTML = content2;
+
+printObjectArray(classroom, display);
+
 // //? ************************************************************
+//! chiediamo all'utente con 3 prompt di inserire in un nuovo ogetto
+var newName = prompt("inserisci nome");
+var newSurname = prompt("inserisci cognome");
+var newAge = prompt("inserisci età");
+var newStudent = {
+  name: newName,
+  surname: newSurname,
+  age: newAge,
+};
+
+// inseriamo il nuovo stutante nell'array di oggetti classroom...... non appare in pagina perchè la stampa e prima della creazione del nuovo studente
+//creiamo una funzione per stampare
+
+classroom.push(newStudent);
+
+console.table(classroom);
+printObjectArray(classroom, display);
+
+//! FUNCION DECLARATION
+/**
+ * Stampa in pagina un array
+ * @param {array} //Array di oggetti
+ * @param {HTMLElement} //id elemento html
+ */
+function printObjectArray(objectArray, targetElement) {
+  var content = "";
+  for (var i = 0; i < objectArray.length; i++) {
+    var currentItem = objectArray[i];
+    content += "<li>";
+    for (var key in currentItem) {
+      content += "<span>" + key + ": </span>" + currentItem[key] + "<br/>";
+    }
+    content += "</li>";
+  }
+  targetElement.innerHTML = content;
+}
